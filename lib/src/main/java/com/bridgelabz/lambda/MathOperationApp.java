b6648f4ff2d08038f457e51c2cfffc9bd99f91fb
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-13 19:19:03 +0530

    ADDED: method that receives lambda function as parameter

--- lib/src/main/java/com/bridgelabz/lambda/MathOperationApp.java
+++ lib/src/main/java/com/bridgelabz/lambda/MathOperationApp.java
@@ -3,6 +3,9 @@ package com.bridgelabz.lambda;
 @FunctionalInterface
 interface IMathFunction{
 	int calculate(int a, int b);
+	static void printResult(int a, int b, String function, IMathFunction fObj) {
+		System.out.println("Result of "+function+" is "+fObj.calculate(a, b));
+	}
 }
 
 public class MathOperationApp {
@@ -10,9 +13,22 @@ public class MathOperationApp {
 		
 		IMathFunction add = (x, y) -> (x+y);
 		IMathFunction subtract = (x, y) -> (x-y);
-		IMathFunction divide = (int x, int y) -> x/y;
+		IMathFunction multiply = (x, y) -> x*y;
+		IMathFunction divide = (int x, int y) -> {
+			if(y==0) return 0;
+			return x/y;
+		};
+		
+		
 		System.out.println("Addition is : " + add.calculate(6, 3));
 		System.out.println("Subtraction is : "+ subtract.calculate(6, 3));
+		System.out.println("Multiplication is : "+ multiply.calculate(6, 3));
 		System.out.println("Division is : "+ divide.calculate(6, 3));
+		
+		
+		IMathFunction.printResult(6, 3, "addition", add);
+		IMathFunction.printResult(6, 3, "subtraction", subtract);
+		IMathFunction.printResult(6, 3, "Multiplication", multiply);
+		IMathFunction.printResult(6, 3, "division", divide);
 	}
 }
